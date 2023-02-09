39cefc2779aa76be124ed7da3a91f4ad3d1d0fea
Julius Stenbæk Christoffersen <julius@adnudging.com>
2023-02-09 16:06:06 +0100

    Moved files out of node_parser. Added dotnet parser

--- 02._Data_Formats/homework/node_parser/index.js
+++ 02._Data_Formats/homework/node_parser/index.js
@@ -1,31 +1,31 @@
 import * as Parser from "./fileParser.js";
 
 // Parsing a CSV file
-let parsedCsv = Parser.parse("./files/me.csv");
+let parsedCsv = Parser.parse("./../files/me.csv");
 console.log("Parsed CSV: ");
 console.log(parsedCsv[0]);
 console.log();
 
 // Parsing a JSON file
-let parsedJson = Parser.parse("./files/me.json");
+let parsedJson = Parser.parse("./../files/me.json");
 console.log("Parsed JSON: ");
 console.log(parsedJson);
 console.log();
 
 // Parsing an XML file
-let parsedXml = Parser.parse("./files/me.xml");
+let parsedXml = Parser.parse("./../files/me.xml");
 console.log("Parsed XML: ");
 console.log(parsedXml);
 console.log();
 
 // Parsing a YAML file
-let parsedYaml = Parser.parse("./files/me.yaml");
+let parsedYaml = Parser.parse("./../files/me.yaml");
 console.log("Parsed YAML: ");
 console.log(parsedYaml);
 console.log();
 
 // Parsing a TXT file
-let parsedTxt = Parser.parse("./files/me.txt");
+let parsedTxt = Parser.parse("./../files/me.txt");
 console.log("Parsed TXT: ");
 console.log(parsedTxt);
 console.log();
