152fc179be02f45a3d86cadaa5d19195c729c7e3
Julius Stenbæk Christoffersen <julius@adnudging.com>
2023-02-09 16:09:55 +0100

    Moved files out of node_parser. Added dotnet_parser

---
+++ 02._Data_Formats/homework/dotnet_parser/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program {
+  public static void Main(string[] args) {
+    Console.WriteLine("Parsed CSV:");
+    Console.WriteLine(FileParser.Parse("./../files/me.csv"));
+    Console.WriteLine();
+
+    Console.WriteLine("Parsed JSON:");
+    Console.WriteLine(FileParser.Parse("./../files/me.json"));
+    Console.WriteLine();
+
+    Console.WriteLine("Parsed XML:");
+    Console.WriteLine(FileParser.Parse("./../files/me.xml"));
+    Console.WriteLine();
+
+    Console.WriteLine("Parsed YAML:");
+    Console.WriteLine(FileParser.Parse("./../files/me.yaml"));
+    Console.WriteLine();
+
+    Console.WriteLine("Parsed TXT:");
+    Console.WriteLine(FileParser.Parse("./../files/me.txt"));
+    Console.WriteLine();
+  } 
+}
